418d01a93e36adb8817e94c494095cc9b5f7ca09
Mahmudur <mahmudhera93@gmail.com>
2022-09-23 03:13:08 -0400

    bug fixes

--- test_c_code/solver.c
+++ test_c_code/solver.c
@@ -9,11 +9,11 @@ double *random_matrix(int m, int n)
 /* Creates a random sparse-ish matrix for test purposes. In each column, we fill in
    about 1/10 of the entries with nonzero elements. */
 {
-    double *A = malloc(m*n,sizeof(double));
+    double *A = malloc(m*n*sizeof(double));
     int i,j;
-    for(j=0;j<n;j++) {
-        for(i=0;i<ceil((double)(m)/10.0);i++) {
-            A[(rand() % Msize) + Msize*j] = 2.0*((double)(rand())/(double)(RAND_MAX)) - 1.0;
+    for(j = 0; j < n; j++) {
+        for(i = 0; i < ceil((double)(m)/10.0); i++) {
+            A[(rand() % m) + m*j] = 2.0*((double)(rand())/(double)(RAND_MAX)) - 1.0;
         }
     }
     return A;
@@ -41,7 +41,7 @@ int main()
     taucs_print_ccs_matrix(mat_cc);
 
     mat_cc_transpose = taucs_ccs_transpose(mat_cc);
-    printf("Now printing the transpose...\n")
+    printf("Now printing the transpose...\n");
     taucs_print_ccs_matrix(mat_cc_transpose);
 
     return 0;
