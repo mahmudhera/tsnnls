ac539b88bd67fa5c0596d7c7f6cd6809d005d6a1
Mahmudur <mahmudhera93@gmail.com>
2022-09-23 05:09:34 -0400

    test

--- test_c_code/solver.c
+++ test_c_code/solver.c
@@ -48,16 +48,16 @@ int main()
 {
     //test_basic();
     int i;
-    double A[30] = {1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-                    0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
-                    0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-                    0.0, 0.0, 0.0, 1.0, 0.0, 1.0,
-                    0.0, 0.0, 0.0, 0.0, 1.0, 0.0};
+    double A[20] = {1.0, 0.0, 0.0, 0.0,
+                    0.0, 0.0, 1.0, 0.0,
+                    0.0, 1.0, 0.0, 0.0,
+                    0.0, 0.0, 0.0, 1.0,
+                    1.0, 0.0, 1.0, 0.0};
 
-    taucs_ccs_matrix *taucs_A = taucs_construct_sorted_ccs_matrix(A, 5, 6);
+    taucs_ccs_matrix *taucs_A = taucs_construct_sorted_ccs_matrix(A, 5, 4);
     taucs_print_ccs_matrix(taucs_A);
 
-    double b[6] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+    double b[5] = {1.1, 2.2, 3.3, 4.4, 3.3};
     taucs_double *taucs_b = b;
 
     taucs_double *result = t_snnls(taucs_A, taucs_b, NULL, 1, 0);
